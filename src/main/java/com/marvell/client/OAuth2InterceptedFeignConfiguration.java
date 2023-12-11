a3fbc85584631519af8dadd9b0a75a15a1086649
jesmanwicttorff <jwarteaga@hotmail.com>
2023-12-11 14:17:14 -0300

    marvel backend

---
+++ src/main/java/com/marvell/client/OAuth2InterceptedFeignConfiguration.java
@@ -0,0 +1,13 @@
+package com.marvell.client;
+
+import com.marvell.security.oauth2.AuthorizationHeaderUtil;
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+
+public class OAuth2InterceptedFeignConfiguration {
+
+    @Bean(name = "oauth2RequestInterceptor")
+    public RequestInterceptor getOAuth2RequestInterceptor(AuthorizationHeaderUtil authorizationHeaderUtil) {
+        return new TokenRelayRequestInterceptor(authorizationHeaderUtil);
+    }
+}
---
+++ src/main/java/com/marvell/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.marvell.repository;
+
+import com.marvell.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
